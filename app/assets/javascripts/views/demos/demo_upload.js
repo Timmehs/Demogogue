93bd7519242fd655a1415472b41961318d58ef4e
Tim Sandberg <tasandberg@gmail.com>
2014-12-01 10:51:23 -0800

    Add image form modal, upload form hide/show animations, upload successfully saves audio_url

--- app/assets/javascripts/views/demos/demo_upload.js
+++ app/assets/javascripts/views/demos/demo_upload.js
@@ -2,8 +2,7 @@ Demogogue.Views.DemoUploadView = Backbone.View.extend({
   template: JST['demos/upload'],
   className: "upload-view",
   events: {
-    "click button" : "click",
-    "click button#cancel-btn" : "cancel"
+    "click button#upload-button" : "uploadAudio",
   },
 
   initialize: function(options) {
@@ -16,10 +15,28 @@ Demogogue.Views.DemoUploadView = Backbone.View.extend({
     return this;
   },
 
-  click: function(event) {
+  uploadAudio: function(event) {
     event.preventDefault();
     var bucket = new AWS.S3({ params: { Bucket: "demogogue"}});
     var file = document.getElementById('file-chooser').files[0];
+
+    if (file) {
+      var params = this.parseFile(file);
+      var req = bucket.putObject(params, function (err, data) {
+        err ? console.log("Error") : console.log("Uploaded.");
+      });
+      // Progress Bar
+      this.renderProgressBar(req);
+      this.audioSuccessHandler(req);
+      req.send();
+      $('#demoTitle').val(file.name.replace(/\.[^/.]+$/, ""));
+
+    } else {
+      alert('No file selected');
+    }
+  },
+
+  parseFile: function(file) {
     var params = {
       Key: file.name,
       ContentType: file.type,
@@ -27,38 +44,37 @@ Demogogue.Views.DemoUploadView = Backbone.View.extend({
       ACL: 'public-read'
     };
 
-    if (file) {
-      $(".progress").show();
-      var req = bucket.putObject(params, function (err, data) {
-        var msg = err ? "Error" : "Uploaded.";
-      });
-
-      // Progress Bar
-      var lastPercentage = 0;
-      req.on('httpUploadProgress', function(progress) {
-        console.log('progress');
-        var percentage = Math.floor((progress.loaded / progress.total) * 100);
-        if (percentage > lastPercentage) {
-          lastPercentage = percentage;
-          if (percentage > 10) {
-            $('.progress-bar').html(percentage + "%");
-          }
-          $('.progress-bar').css("width", percentage + "%").val(percentage + "%");
-        } else if (lastPercentage === 100) {
-          $('.progress-bar').css('background-color', "green")
-            .html('Upload Complete!');
-          $('#cancel-upload').fadeIn();
-        }
-
-      });
-      req.send();
-
-      $('#demoTitle').val(file.name.replace(/\.[^/.]+$/, ""));
-
-
-    } else {
-      alert('No file selected');
-    }
+    return params;
   },
+
+  renderProgressBar: function(request) {
+    var lastPercentage = 0;
+    request.on('httpUploadProgress', function(progress) {
+      $(".progress").show();
+      var percentage = Math.floor((progress.loaded / progress.total) * 100);
+      if (percentage > lastPercentage) {
+        lastPercentage = percentage;
+        if (percentage > 10) {
+          $('.progress-bar').html(percentage + "%");
+        }
+        $('.progress-bar').css("width", percentage + "%").val(percentage + "%");
+      }
+    });
+  },
+
+  audioSuccessHandler: function(request) {
+    var newDemo = this.demo;
+    request.on('success', function (response) {
+      $('.progress-bar').css('background-color', "#00CD00")
+      .html('<div style="display:none;" id="complete">Upload Complete!</div>');
+      var audioUrl = response.request.httpRequest.stream.responseURL;
+      newDemo.set("audio_url", audioUrl);
+      $('.upload-form-form').slideUp();
+      $('.demo-form').slideDown();
+      setTimeout(function() {
+        $('#complete').fadeIn();
+      }, 350);
+    })
+  }
 
 });
